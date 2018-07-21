915d51a3822bf49c48eecbd9c19442887866ad59
Hrishikesh <hrishipotdar23@gmail.com>
2018-07-21 09:41:33 +0530

    Transform completed

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -4,10 +4,23 @@ using System.Collections.Generic;
 
 namespace Transform
 {
-    public static class Transform 
+    public delegate int converter(int x);
+
+    public static class Transform
     {
-        public static void Map()
+        public static List<int> Map(this int[] values , converter convert)
         {
+
+            List<int> converted_values = new List<int>();
+            foreach (int item in values)
+            {
+                converted_values.Add(convert(item));
+            }
+
+
+            return converted_values;
+
+
             throw new NotImplementedException();
         }
     }
